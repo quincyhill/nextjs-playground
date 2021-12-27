25cf96270131af0a041b84928d33a5eb7f2cfd5a
quincyhill <therealquincyhill@gmail.com>
2021-12-26 19:27:01 -0600

    Updated the styling, created layout compoennt, implementing things like tailwind and whatnot.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: [
+    // Use *.tsx if using TypeScript
+    './pages/**/*.js',
+    './components/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
