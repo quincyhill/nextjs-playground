0697412f1e742fa60a63c187f19af3420d5b74ab
quincyhill <therealquincyhill@gmail.com>
2022-01-02 21:48:53 -0600

    Optimized images

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,20 @@
-import { useState } from 'react';
-import Head from 'next/head';
+import { useState } from 'react'
+import Head from 'next/head'
+import Image from 'next/image'
 
-import Fuse from 'fuse.js';
-import _ from 'lodash';
+import Fuse from 'fuse.js'
+import _ from 'lodash'
 
-import styles from '../styles/Home.module.css';
-import CodeSampleModal from '../components/CodeSampleModal';
+import styles from '../styles/Home.module.css'
+import CodeSampleModal from '../components/CodeSampleModal'
 
 export default function Start({ countries }) {
-  const [results, setResults] = useState(countries);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [results, setResults] = useState(countries)
+  const [isModalOpen, setIsModalOpen] = useState(false)
   const fuse = new Fuse(countries, {
     keys: ['name'],
     threshold: 0.3,
-  });
+  })
 
   return (
     <div>
@@ -33,7 +34,12 @@ export default function Start({ countries }) {
         </h1>
 
         <div className={styles.heroImage}>
-          <img src="large-image.jpg" alt="Large Image" />
+          <Image
+            src="/large-image.jpg"
+            alt="Large Image"
+            width={3048}
+            height={2024}
+          />
         </div>
 
         <div>
@@ -43,21 +49,21 @@ export default function Start({ countries }) {
             placeholder="Country search..."
             className={styles.input}
             onChange={async (e) => {
-              const { value } = e.currentTarget;
+              const { value } = e.currentTarget
 
               const searchResult = fuse
                 .search(value)
-                .map((result) => result.item);
+                .map((result) => result.item)
 
               const updatedResults = searchResult.length
                 ? searchResult
-                : countries;
-              setResults(updatedResults);
+                : countries
+              setResults(updatedResults)
 
               // Fake analytics hit
               console.info({
                 searchedAt: _.now(),
-              });
+              })
             }}
           />
 
@@ -91,17 +97,17 @@ export default function Start({ countries }) {
         >
           Powered by
           <span className={styles.logo}>
-            <img src="/vercel.svg" alt="Vercel Logo" />
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  );
+  )
 }
 
 export async function getServerSideProps() {
-  const response = await fetch('https://restcountries.com/v3.1/all');
-  const countries = await response.json();
+  const response = await fetch('https://restcountries.com/v3.1/all')
+  const countries = await response.json()
 
   return {
     props: {
@@ -111,5 +117,5 @@ export async function getServerSideProps() {
         population: country.population,
       })),
     },
-  };
+  }
 }
