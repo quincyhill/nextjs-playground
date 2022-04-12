b0ecc5e5d15c1ec1ff77d278c090a4480a473414
quincyhill <therealquincyhill@gmail.com>
2022-04-11 20:08:27 -0500

    Added an action and working on building out the reducer

--- lib/store.ts
+++ lib/store.ts
@@ -1,19 +1,26 @@
 import { configureStore } from '@reduxjs/toolkit'
 
-type ColorType = 'red' | 'blue' | 'yellow' | 'green' | 'orange' | 'purple'
+type ColorChoice =
+  | 'red'
+  | 'blue'
+  | 'yellow'
+  | 'green'
+  | 'orange'
+  | 'purple'
+  | null
 
 interface TodoItem {
   id: number
   text: string
   completed: boolean
-  color: ColorType
+  color: ColorChoice
 }
 
 type StatusType = 'all' | 'active' | 'completed'
 
 interface TodoFilter {
   status: StatusType
-  colors: ColorType[]
+  colors: ColorChoice[]
 }
 
 interface TodoAppState {
@@ -21,6 +28,7 @@ interface TodoAppState {
   filter: TodoFilter
 }
 
+// Could eventaully refactor the payload to have both the text and color but i'm just doing color for now
 interface TodoAddedAction {
   type: 'todos/todoAdded'
   payload: string
@@ -32,6 +40,11 @@ interface TodoToggledAction {
 }
 
 interface TodoColorSelectedAction {
+  type: 'todos/todoColorSelected'
+  payload: number
+}
+
+interface TodoDeletedAction {
   type: 'todos/todoDeleted'
   payload: number
 }
@@ -53,14 +66,15 @@ interface TodoStatusFilterChangedAction {
 
 interface TodoColorFilterChangedAction {
   type: 'todos/todoColorFilterChanged'
-  // this may need changing but run with this for now
-  payload: ColorType[]
+  // NOTE: this may need changing but run with this for now
+  payload: ColorChoice[]
 }
 
 type Action =
   | TodoAddedAction
   | TodoToggledAction
   | TodoColorSelectedAction
+  | TodoDeletedAction
   | TodoAllCompletedAction
   | TodoCompletedClearedAction
   | TodoStatusFilterChangedAction
@@ -78,27 +92,87 @@ const initialState: TodoAppState = {
   },
 }
 
+function nextTodoId(todos: TodoItem[]): number {
+  const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
+  return maxId + 1
+}
+
 function appReducer(
   state: TodoAppState = initialState,
   action: Action
 ): TodoAppState {
+  // Only immutable updates allowed
+  // No side effects aka anything that is a change to a state that is not within the bounds of the state
+  // from only state and action arguments
+  // no saving files, logging to console http requests, generating random numbers, etc
   switch (action.type) {
     case 'todos/todoAdded':
-      return state
+      // All this logic could be in the return or outisde, just preference
+      // We need to return a new state object
+      // Make sure to make a copy of EVERY level of nesting that needs to be updated, aka alot of destructuring / spread operations
+      return {
+        // That has all the existing state data
+        ...state,
+        // But has a new arrow for the `todos` field
+        todos: [
+          // with all of the old todos
+          ...state.todos,
+          // and the new todo object
+          {
+            // Use an auto-incrementing numeric ID for this example
+            id: nextTodoId(state.todos),
+            text: action.payload,
+            completed: false,
+            color: null,
+          },
+        ],
+      }
     case 'todos/todoToggled':
-      return state
+      return {
+        // Again copy the entire state object
+        ...state,
+        // This time, we need to make a copy of the old todos array
+        todos: state.todos.map((todo) => {
+          // If this isn't the todo we're looking for, leave it alone
+          if (todo.id !== action.payload) {
+            return todo
+          }
+          // We've found the todo that has to change. Return a copy:
+          return {
+            ...todo,
+            // Flip the completed value
+            completed: !todo.completed,
+          }
+        }),
+      }
+    case 'todos/todoColorSelected':
+      return {
+        ...state,
+      }
     case 'todos/todoDeleted':
-      return state
+      return {
+        ...state,
+      }
     case 'todos/todoAllCompleted':
-      return state
+      return {
+        ...state,
+      }
     case 'todos/todoCompletedCleared':
-      return state
+      return {
+        ...state,
+      }
     case 'todos/todoStatusFilterChanged':
-      return state
+      return {
+        ...state,
+      }
     case 'todos/todoColorFilterChanged':
-      return state
+      return {
+        ...state,
+      }
     default:
-      return state
+      return {
+        ...state,
+      }
   }
 }
 
